79298408738cf6ef5d9d97e4b8fa63de67cf4545
Souza <studioswhirl@gmail.com>
2025-06-13 22:35:46 -0300

    It will be published in a few hours

---
+++ schema/channelVar.js
@@ -0,0 +1,11 @@
+
+const mongoose = require('mongoose');
+const { Schema } = require('mongoose');
+
+const channelVarSchema = new Schema({
+  channelId: String,
+  variable: String,
+  value: Schema.Types.Mixed,
+});
+
+module.exports = mongoose.model('ChannelVar', channelVarSchema);
